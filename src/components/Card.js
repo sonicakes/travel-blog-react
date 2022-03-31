e4c36f3bab0d0caa8e604c79cf0081e0104a3cdc
unknown <soniaparker1207@gmail.com>
2022-04-01 10:04:05 +1100

    finish styling Card components

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,15 +5,21 @@ export default function Card(props) {
     return (
         <div className="card">
             <div className='card__left side'>
-                <img src={props.card.imageUrl}></img>
-            </div>
-            <div className='card__right side'>
-                <div className='heading'>
-                    <div className='location'><img src={pointer}></img>{props.card.location}</div> <a href={props.card.googleMapsUrl} target="_blank">View on Google Maps</a>
+                <div className='wrapper-portrait'>
+                    <img src={props.card.imageUrl}></img>
                 </div>
-                <h1>{props.card.title}</h1>
-                <div>{props.card.startDate} - {props.card.endDate}</div>
-                <p>{props.card.description}</p>
+            </div>
+
+            <div className='card__right side'>
+                <div className='info'>
+                    <div className='heading'>
+                        <div className='location'><img src={pointer}></img>{props.card.location}</div>
+                        <a href={props.card.googleMapsUrl} target="_blank">View on Google Maps</a>
+                    </div>
+                    <h1>{props.card.title}</h1>
+                    <div className="dates">{props.card.startDate} - {props.card.endDate}</div>
+                    <p>{props.card.description}</p>
+                </div>
             </div>
         </div>
     )
